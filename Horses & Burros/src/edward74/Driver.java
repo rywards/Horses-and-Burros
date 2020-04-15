3132553104a7d355d0c86b8d10994e96fe7af740
rywar <rywar@DESKTOP-VMV22IR>
2020-04-15 13:53:16 -0400

    Added the method stubs for loading and displaying statistics.

--- Horses & Burros/src/edward74/Driver.java
+++ Horses & Burros/src/edward74/Driver.java
@@ -1,11 +1,40 @@
 package edward74;
-
+import java.io.File;
+import java.io.BufferedReader;
 // Driver for the herd management program.
+/**
+ * @author Ryan Edwards
+ * @version 4.15.2020
+ */
 public class Driver {
 
 	public static void main(String[] args) {
 		
-
+		BufferedReader buff = new BufferedReader(file);
+		DataSet animalInfo = new DataSet();
+		
+		
+	}
+	
+	/**
+	 * Method to load statistics from the file. Places each
+	 * row of data into a state with proper attributes.
+	 * 
+	 * @param data
+	 * @param file
+	 * @param numRows
+	 */
+	public void loadStatistics(DataSet data, File file, int numRows) {
+		
+		return;
+	}
+	
+	/**
+	 * Method to display statistics for each state
+	 */
+	public void displayStatistics() {
+		
+		
 	}
 
 }
